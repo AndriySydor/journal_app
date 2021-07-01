16a6a0a961e609fa4601e6be96c2d87268257bc7
AndriySydor <44542945+AndriySydor@users.noreply.github.com>
2021-07-01 14:26:03 +0300

    Update settings.py

--- theJournal/theJournal/settings.py
+++ theJournal/theJournal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-do*3ntplabb=2@$_%=#*5&(@+p24h+tywt00zdmzxm!gh%vv0p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
