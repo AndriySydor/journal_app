df103654eb2fa07901676a91019cc5cfb2a4b0a6
AndriySydor <sidorandriy231@gmail.com>
2021-07-05 16:58:20 +0300

    add .gitignore

--- theJournal/theJournal/settings.py
+++ theJournal/theJournal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-do*3nkfjgksdjfkjgfkgfkjgglkdjglk!!#@##e*&^%%SDF@##!!#koirwjiefD3ER$@(%&#0zdmzxm!gh%vv0p'
+SECRET_KEY = 'django-insecure-dEKFEJRnnjjjjjkklkkjsrlkejrejrkwljlk3RJKRHRJ#*&#$%^%#$&#827340(EShgfzxm!gh%vv0p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
